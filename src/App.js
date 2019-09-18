b506a5428399dc5f0921c5a140edda593a2ceebb
Sylvie LeDuc <Sylvie@Sylvies-MacBook-Pro.local>
2019-09-18 12:04:00 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FLORIDA</h1>
+      <img alt="Texas showing major cities." src="https://www.nationsonline.org/maps/USA/Florida_map.jpg" width="400"/>
+      <img className="blue-border" alt="Houses on water" src="https://odis.homeaway.com/odis/destination/ce6c81d5-1700-4d8e-a5f7-17c5e5e54852.hw5.jpg" width="400"/>
+    </div>
+  );
+}
+
+export default App;
